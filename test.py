6ae4401cedd24308004e3d74af7a46659d63ad13
winky <winky@winky.sun>
2024-11-08 17:35:21 +0800

    sl

---
+++ test.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf8 -*-
+print("com.dbexchange")
+print("com.xxl.job")
+print("com.bv")
+print("汉字测试文件外发")
+print("test")
+print("AccessKeySecret")
+print("AccessKeyId")
+print("bhServer")
